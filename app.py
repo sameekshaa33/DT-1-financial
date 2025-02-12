3968dcc68f9c1d1a2c808fed1d23ed40517b2bec
sameekshaa33 <sameekshaa.33@gmail.com>
2025-02-12 21:20:58 +0530

    project

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request,jsonify
+from test import TextToNum
+import pickle
 
 app = Flask(__name__)
 
@@ -11,6 +13,21 @@ def predict():
     if request.method == "POST":
         msg = request.form.get("message")  
         print("Received message:", msg)  
+        ob=TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st=ob.stemme()
+        with open("vectorizer.pickle","rb")as vcfile:
+            vc=pickle.load(vcfile)
+        stvc=" ".join(st)#joining stemmed data--as it is word by word
+        data=vc.transform([stvc])
+        print(data)
+        with open("model.pickle","rb") as mbfile:
+            model=pickle.load(mbfile)
+        pred=model.predict(data)
+        return jsonify({"result":str(pred[0])})
+
     else:
         return render_template("predict.html") # Ensure it returns a response#till now we can get the data from frontend to the backend 
 
