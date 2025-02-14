a88cd547a6d3d457f023e2821c24acc0c8755311
sameekshaa33 <sameekshaa.33@gmail.com>
2025-02-14 11:20:21 +0530

    hy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 from test import TextToNum
 import pickle
 
@@ -13,24 +13,23 @@ def predict():
     if request.method == "POST":
         msg = request.form.get("message")  
         print("Received message:", msg)  
-        ob=TextToNum(msg)
+        ob = TextToNum(msg)
         ob.cleaner()
         ob.token()
         ob.removeStop()
-        st=ob.stemme()
-        with open("vectorizer.pickle","rb")as vcfile:
-            vc=pickle.load(vcfile)
-        stvc=" ".join(st)#joining stemmed data--as it is word by word
-        data=vc.transform([stvc])
+        st = ob.stemme()
+        with open("vectorizer.pickle", "rb") as vcfile:
+            vc = pickle.load(vcfile)
+        stvc = " ".join(st)  # joining stemmed data--as it is word by word
+        data = vc.transform([stvc])
         print(data)
-        with open("model.pickle","rb") as mbfile:
-            model=pickle.load(mbfile)
-        pred=model.predict(data)
-        return jsonify({"result":str(pred[0])})
-
+        with open("model.pickle", "rb") as mbfile:
+            model = pickle.load(mbfile)
+        pred = model.predict(data)
+        sentiment = str(pred[0])
+        return render_template("result.html", sentiment=sentiment)
     else:
-        return render_template("predict.html") # Ensure it returns a response#till now we can get the data from frontend to the backend 
+        return render_template("predict.html")  # Ensure it returns a response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)  # Added debug mode for better error tracking
-
